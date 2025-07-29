7e711f647d9636ca1feea9964e6f6ee705425b4b
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-07-29 13:46:46 +0530

    Create Triangle1.cpp

---
+++ Triangle1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int j;
+    int k;
+for(i=0;i<4;i++)
+{
+ 
+    for(k=0;k<=i;k++)
+    {
+         cout<<"*";
+    }
+    
+  cout<<endl;
+}
+}
+
+//Output
+  *
+  **
+  ***
+  ****
